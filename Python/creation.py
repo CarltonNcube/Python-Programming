f7a7f8dfdebc2ab4111205d3ea91e36cf3e52942
CarltonNcube <carltonncube@outlook.com>
2023-07-16 08:49:08 +0000

    String creation using single, double, triple and multi-line

---
+++ Python/creation.py
@@ -0,0 +1,25 @@
+#!bin/bash/Python3
+
+#Python program for creation of a string
+#creating s string with single qoutes
+string1 = 'Welcome to python programming'
+print("String with single qoutes: ")
+print(string1)
+
+#Creating a string with double qoutes
+string2 = "i'm a Pythonista"
+print("\nString with double qoutes: ")
+print(string2)
+
+#Creating a string with triple qoutes
+string3 = '''I'm a Pythonista for life'''
+print("\nString with triple qoutes: ")
+print(string3)
+
+#Creating a string with triple qoutes
+#allows multiple lines
+string4 = '''Python 
+	     For
+	     Life'''
+print("\nCreating a multi-line string: ")
+print(string4)
