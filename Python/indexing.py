864c45c2d5b33da3921abd30f10add6893c5898c
CarltonNcube <carltonncube@outlook.com>
2023-07-16 09:57:13 +0000

    accessing characters in Python string using the indexing method

---
+++ Python/indexing.py
@@ -0,0 +1,13 @@
+#Python program to access a characters of a string
+
+string1 = "Pythonist for life"
+print("Initial string: ")
+print(string1)
+
+#Print first character
+print("\nFirst character of string is: ")
+print(string1[0])
+
+#Print last character
+print("Last character of string is: ")
+print(string1[-1])
