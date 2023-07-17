ea3b91f2bd537edf7bf5c6c04277ae8a056f5f8d
CarltonNcube <carltonncube@outlook.com>
2023-07-17 21:21:54 +0000

    Updating entire string

---
+++ Python/updatingstring.py
@@ -0,0 +1,12 @@
+#Python pragram to update
+#entire string
+
+string1 = "Hello, I'm a Pythonista"
+print("Inital string: ")
+print(string1)
+
+#Updating a string
+string1 "Welcome to Python programming"
+print("\nUpdated string: ")
+print(string1)
+
