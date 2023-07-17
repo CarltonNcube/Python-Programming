7cd384f1017c489a1932d0545de37db706907985
CarltonNcube <carltonncube@outlook.com>
2023-07-17 20:48:31 +0000

    slicing in a string done by slicing operator (colon)

---
+++ Python/slicing.py
@@ -0,0 +1,17 @@
+#Python program to demostrate
+#slicing a string
+
+#creating a string
+string1 = "PythonistaForLife"
+print("Initial string ")
+print(string1)
+
+#Printing 3rd to 12th character
+print("\nSlicing character from 3-12: ")
+print(string1[3:12])
+
+#Printing character between
+#3rd and 2nd last character
+print("\nSlicing character between " +
+      "3rd and 2nd last character: ")
+print(string1[3:-2])
