891995c8aea54ec1ccc48bc465af4798638ab87a
CarltonNcube <carltonncube@outlook.com>
2023-07-17 21:43:42 +0000

    deleting entire string using del

---
+++ Python/deletingentire_string.py
@@ -0,0 +1,12 @@
+#Python program to delete
+#entire string
+
+string1 = "Hello, I'm a Pythonista"
+print("Initial string: ")
+print(string1)
+
+#Deleting a string
+#with the use of del
+del string1
+print("\nDeleting entire string: ")
+print(string1)
